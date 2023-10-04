bb2a62489c601afd9eb1b3846f06b80cc257c596
ak falah <akfalah@192.168.1.15>
2023-10-04 18:08:46 +0700

    jobsheet 4

---
+++ 04_AhmadKhoirulFalah_RelasiKelas/Jobsheet4/src/falah/relasiclass/percobaan4/Penumpang.java
@@ -0,0 +1,34 @@
+package falah.relasiclass.percobaan4;
+
+public class Penumpang {
+    private String ktp;
+    private String nama;
+
+    Penumpang(String ktp, String nama) {
+        this.ktp = ktp;
+        this.nama = nama;
+    }
+
+    public void setKtp(String ktp) {
+        this.ktp = ktp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKtp() {
+        return ktp;
+    }
+
+    public String info() {
+        String info = "";
+        info += "Ktp: " + ktp + "\n";
+        info += "Nama: " + nama + "\n";
+        return info;
+    }
+}
